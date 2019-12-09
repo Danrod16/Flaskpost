dddcc46c7845c506a109559f4b6743e814f8549b
Martin Potie <marti15@hotmail.com>
2019-12-09 17:18:12 +0100

    FEAT: added applications model and added names to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_140826) do
+ActiveRecord::Schema.define(version: 2019_12_09_161559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.string "field"
+    t.string "job_title"
+    t.string "contract_types"
+    t.string "experience"
+    t.string "languages"
+    t.string "locations"
+    t.string "description"
+    t.integer "salary_min"
+    t.integer "salary_max"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_applications_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +39,11 @@ ActiveRecord::Schema.define(version: 2019_12_09_140826) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "applications", "users"
 end
