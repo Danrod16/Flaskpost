fe2ddf6ea4468c7a36a9c6cd924748b3ca193b6f
Martin Potie <marti15@hotmail.com>
2019-12-09 17:30:16 +0100

    FEAT: added company foreign key in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_162722) do
+ActiveRecord::Schema.define(version: 2019_12_09_162915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,8 @@ ActiveRecord::Schema.define(version: 2019_12_09_162722) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.bigint "company_id"
+    t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -93,4 +95,5 @@ ActiveRecord::Schema.define(version: 2019_12_09_162722) do
   add_foreign_key "messages", "matches"
   add_foreign_key "messages", "users"
   add_foreign_key "postings", "companies"
+  add_foreign_key "users", "companies"
 end
