fe7f4b758860aa2b86b0bf436bec735959cde199
Kevin Liebholz <kevin@liebholz.eu>
2019-12-10 11:50:57 +0100

    FEAT: changed languages, contract_types, locations in table postings & applications to arrays

--- db/migrate/20191209161559_create_applications.rb
+++ db/migrate/20191209161559_create_applications.rb
@@ -3,10 +3,10 @@ class CreateApplications < ActiveRecord::Migration[5.2]
     create_table :applications do |t|
       t.string :field
       t.string :job_title
-      t.string :contract_types
+      t.string :contract_types, array: true
       t.string :experience
-      t.string :languages
-      t.string :locations
+      t.string :languages, array: true
+      t.string :locations, array: true
       t.string :description
       t.integer :salary_min
       t.integer :salary_max
--- db/migrate/20191209162259_create_postings.rb
+++ db/migrate/20191209162259_create_postings.rb
@@ -3,10 +3,10 @@ class CreatePostings < ActiveRecord::Migration[5.2]
     create_table :postings do |t|
       t.string :field
       t.string :job_title
-      t.string :contract_types
+      t.string :contract_types, array: true
       t.string :experience
-      t.string :languages
-      t.string :locations
+      t.string :languages, array: true
+      t.string :locations, array: true
       t.string :description
       t.integer :salary_max
       t.references :company, foreign_key: true
