901e00747462b34d5ed40579bdd66f002ce22cf4
Kevin Liebholz <kevin@liebholz.eu>
2019-12-10 11:44:13 +0100

    FEAT: adding company and posting routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :company, only: [:new, :create]
   resources :applications, only: [:index, :new, :create, :edit, :update, :delete]
+  resources :postings, only: [:index, :new, :create, :edit, :update, :delete]
+
   resources :messages, only: [:create]
   resources :matches, only: [:index, :show, :create, :update]
 end
