269cdc41c9103ebd71dab1c9935504bead587218
Kevin Liebholz <kevin@liebholz.eu>
2019-12-10 16:55:45 +0100

    FEAT: creating postings#new incl. view

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -7,4 +7,31 @@ class PostingsController < ApplicationController
   def new
     @posting = Posting.new
   end
+
+  def create
+    @posting = Posting.new(posting_params)
+    @posting.company = current_user.company
+
+    raise
+    if @posting.save
+      redirect_to postings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def posting_params
+    params.require(:posting).permit(
+      :field,
+      :job_title,
+      :contract_types,
+      :experience,
+      :languages,
+      :locations,
+      :description,
+      :salary_max
+    )
+  end
 end
