1d0b92e906d91372bf07bce5593c82a2e071bb99
Martin Potie <marti15@hotmail.com>
2019-12-10 15:30:38 +0100

    FIX: corrected plurality in addcompanytousers file

--- db/migrate/20191209162915_add_company_to_users.rb
+++ db/migrate/20191209162915_add_company_to_users.rb
@@ -1,4 +1,4 @@
-class AddCompanyToUser < ActiveRecord::Migration[5.2]
+class AddCompanyToUsers < ActiveRecord::Migration[5.2]
   def change
     add_reference :users, :company, foreign_key: true
   end
