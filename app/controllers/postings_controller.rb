fbeca2dc4ced3428dce0d47713c8a6cf37bbb5d7
Kevin Liebholz <kevin@liebholz.eu>
2019-12-11 13:02:58 +0100

    FEAT: spaces in array items can be recognized in saving and retrieving

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -60,5 +60,15 @@ class PostingsController < ApplicationController
     @posting.languages = delete_empty_items(@posting.languages)
     @posting.locations = delete_empty_items(@posting.locations)
     @posting.contract_types = delete_empty_items(@posting.contract_types)
+
+    @posting.languages = escape_spaces(@posting.languages)
+    @posting.locations = escape_spaces(@posting.locations)
+    @posting.contract_types = escape_spaces(@posting.contract_types)
+  end
+
+  def escape_spaces(arr)
+    arr.map do |item|
+      item.gsub(/\s/, "\s")
+    end
   end
 end
