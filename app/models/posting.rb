703f490ad1c779e1de79aac2e9faa36a4d5d3996
Kevin Liebholz <kevin@liebholz.eu>
2019-12-11 14:40:01 +0100

    FIX: changed validation allow_blanc to allow_blank

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -2,14 +2,14 @@ class Posting < ApplicationRecord
   belongs_to :company
   has_many :matches
 
-  validates :job_title, presence: true, allow_blanc: false
-  validates :field, presence: true, allow_blanc: false
-  validates :contract_types, presence: true, allow_blanc: false
-  validates :experience, presence: true, allow_blanc: false
-  validates :languages, presence: true, allow_blanc: false
-  validates :locations, presence: true, allow_blanc: false
-  validates :description, presence: true, allow_blanc: false
-  validates :salary_max, presence: true, allow_blanc: false
+  validates :job_title, presence: true, allow_blank: false
+  validates :field, presence: true, allow_blank: false
+  validates :contract_types, presence: true, allow_blank: false
+  validates :experience, presence: true, allow_blank: false
+  validates :languages, presence: true, allow_blank: false
+  validates :locations, presence: true, allow_blank: false
+  validates :description, presence: true, allow_blank: false
+  validates :salary_max, presence: true, allow_blank: false
 
   validates :description, length: { maximum: 180, minimum: 60 }
   validates :salary_max, numericality: { only_integer: true }
