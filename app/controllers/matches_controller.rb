4a40ffd16fe331cbe3784a30e8de09791d5d7412
Kevin Liebholz <kevin@liebholz.eu>
2019-12-11 16:01:57 +0100

    FEAT: created view for matches#index & method for seeker

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,15 @@
 class MatchesController < ApplicationController
+  def index
+    #-----get all matches for a seeker-----
+    # get all profiles of the user
+    # get all matches of all profiles of the user
+    @matches = []
+
+    profiles = current_user.profiles
+    profiles.each do |profile|
+      profile.matches.each do |match|
+        @matches << match
+      end
+    end
+  end
 end
