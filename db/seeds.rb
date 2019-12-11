7c002e77379e3dd4b81455e911045cf9141f52de
Angus Tatchell <angustatchell@gmail.com>
2019-12-11 12:23:19 +0100

    FEAT: Added Database Clearing Function

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def clear_database
+  puts "CLEARING DATABASE:"
+  puts " "
+
+  puts "Destroying Messages..."
+  Message.destroy_all
+  puts "#{Message.all.count} Messages."
+  puts " "
+
+  puts "Destroying Matches..."
+  Match.destroy_all
+  puts "#{Match.all.count} Matches."
+  puts " "
+
+  puts "Destroying Profiles..."
+  Profile.destroy_all
+  puts "#{Profile.all.count} Profiles."
+  puts " "
+
+  puts "Destroying Postings..."
+  Posting.destroy_all
+  puts "#{Posting.all.count} Postings."
+  puts " "
+
+  puts "Destroying Companies..."
+  Company.destroy_all
+  puts "#{Company.all.count} Companies."
+  puts " "
+
+  puts "Destroying Users..."
+  User.destroy_all
+  puts "#{User.all.count} Users."
+  puts " "
+
+  puts "DATABASE CLEARED."
+  puts " "
+end
+
+clear_database
+
+def seed_database
+  puts "SEEDING DATABASE"
+  puts " "
+
+  puts "Seeding Users..."
+
+end
+
+
+
