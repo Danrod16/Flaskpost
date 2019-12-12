13bb1e1a824eac963a8c0708828df80fa03f5437
Daniel Rodriguez <daniel.rod93@gmail.com>
2019-12-12 15:41:13 +0100

    fix: removed raise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  private
-
-  def after_sign_up_path_for(resource)
-    raise
-    show_cities_path(resource)
-  end
 end
