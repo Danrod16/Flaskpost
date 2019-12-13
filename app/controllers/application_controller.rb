13d6ad051c03d0cca9cd4f4669e018f7ebb2015f
Kevin Liebholz <kevin@liebholz.eu>
2019-12-13 14:58:44 +0100

    INIT & FIX: created helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  def euro_thousand_separate(number)
-  whole_part, decimal_part = number.to_s.split('.')
-  num = [whole_part.gsub(/(\d)(?=\d{3}+$)/, '\1.'), decimal_part].compact.join('.')
-  p "#{num},00"
-  end
 end
