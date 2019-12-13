c9d2d26bdfd6e26dfea55a370d3c26e8703c5513
Angus Tatchell <angustatchell@gmail.com>
2019-12-13 12:53:49 +0100

    updating branch

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -7,9 +7,11 @@ class MatchesController < ApplicationController
     if current_user.company.nil?
       # -----get all matches for a seeker-----
       retrieve_matches(current_user.profiles)
+      # @match_name = Company.find(match.posting.company.id).name
     else
       #-----get all matches for a recruiter-----
       retrieve_matches(current_user.company.postings)
+      # @match_name = User.find(match.profile.user.id).name
     end
   end
 
@@ -23,8 +25,8 @@ class MatchesController < ApplicationController
   end
 
   def retrieve_matches(array)
-    array.each do |p_or_p|
-      p_or_p.matches.each do |match|
+    array.each do |profile_or_posting|
+      profile_or_posting.matches.each do |match|
         @matches << match if match.status == "accepted"
       end
     end
