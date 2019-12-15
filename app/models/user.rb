faae7b7567c2e57dd16758801b174fc2c9f208bf
Daniel Rodriguez <daniel.rod93@gmail.com>
2019-12-15 18:17:39 +0100

    FEAT: added user model auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  devise :omniauthable, omniauth_providers: [:facebook]
   belongs_to :company, optional: true
   has_many :messages
   # has_many :profiles // REPLACED BY BELOW METHOD 'PROFILES'
