8509f3f4f8d746bd7c010260395fe35ce4658aa4
Kevin Liebholz <kevin@liebholz.eu>
2019-12-16 18:34:58 +0100

    CLEAN: added a comment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,6 @@ import "bootstrap";
 // activate the slide function by passing the class "active" to the first card
 import { makeFirstCardActive } from '../swipe/activate_card';
 makeFirstCardActive();
-
+// change the links of the button so it works for the active card
 import { changeButtonLinks } from '../swipe/button_links';
 changeButtonLinks();
