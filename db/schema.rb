f74d4f7a65bd7f6f3fece526a560d37fdfd124f6
Kevin Liebholz <kevin@liebholz.eu>
2019-12-16 10:48:08 +0100

    INIT & FEAT: migration for company avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_152704) do
+ActiveRecord::Schema.define(version: 2019_12_16_094552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_12_15_152704) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "logo"
   end
 
   create_table "matches", force: :cascade do |t|
