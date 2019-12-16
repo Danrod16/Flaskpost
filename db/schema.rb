a27dc3c1dfc6e55449113a2d287aef233b5f7e4b
Kevin Liebholz <kevin@liebholz.eu>
2019-12-16 11:04:05 +0100

    FEAT: migration to remove salary_max for profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_152704) do
+ActiveRecord::Schema.define(version: 2019_12_16_095953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_12_15_152704) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "logo"
   end
 
   create_table "matches", force: :cascade do |t|
@@ -69,7 +70,6 @@ ActiveRecord::Schema.define(version: 2019_12_15_152704) do
     t.string "locations", array: true
     t.string "description"
     t.integer "salary_min"
-    t.integer "salary_max"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
