c00e4def88238540c6ebf9a258a4a7c011218018
Kevin Liebholz <kevin@liebholz.eu>
2019-12-16 15:10:04 +0100

    FEAT: activating the first card in swipe via js - eleminates extracting the first card and doubling HTML parts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
 import "bootstrap";
+
+// ----- swipe functionality -----
+// activate the slide function by passing the class "active" to the first card
+import { makeFirstCardActive } from '../swipe/activate_card';
+makeFirstCardActive();
