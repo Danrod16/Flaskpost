baecc170c9ca4dd74c086d996850dc0cb9b77efb
Daniel Rodriguez <daniel.rod93@gmail.com>
2019-12-17 14:42:46 +0100

    FEAT: empty swiping pull landing done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 import { Visual } from '../purple_cloud'
 
 
+
 // ----- swipe functionality -----
 // activate the slide function by passing the class "active" to the first card
 import { makeFirstCardActive } from '../swipe/activate_card';
@@ -9,3 +10,4 @@ makeFirstCardActive();
 // change the links of the button so it works for the active card
 import { changeButtonLinks } from '../swipe/button_links';
 changeButtonLinks();
+
