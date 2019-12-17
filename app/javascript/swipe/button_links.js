acb9984af7438ab8b21d9779dc91e10e3e168953
Kevin Liebholz <kevin@liebholz.eu>
2019-12-17 10:33:48 +0100

    FIX: making the swipe's profile id setting dynamically

--- app/javascript/swipe/button_links.js
+++ app/javascript/swipe/button_links.js
@@ -4,9 +4,10 @@ const acceptBtn = document.getElementById("accept");
 const declineBtn = document.getElementById("decline");
 
 const baseUrl = window.location.href.split('/')[0] + "//" + window.location.href.split('/')[2]
+const profile_id = window.location.href.split('/')[4]
 
-const acceptUrl = new URL ("/matches/accept_decline?posting_id=jsChange&profile_id=1&user_intention=accepted", baseUrl);
-const declineUrl = new URL ("/matches/accept_decline?posting_id=jsChange&profile_id=1&user_intention=declined", baseUrl);
+const acceptUrl = new URL ("/matches/accept_decline?posting_id=jsChange&profile_id=jsChange&user_intention=accepted", baseUrl);
+const declineUrl = new URL ("/matches/accept_decline?posting_id=jsChange&profile_id=jsChange&user_intention=declined", baseUrl);
 
 const changeButtonLinks = () => {
   $('#carouselExampleCaptions').on('slid.bs.carousel', function (event) {
@@ -15,6 +16,9 @@ const changeButtonLinks = () => {
 
     acceptUrl.searchParams.set( 'posting_id', posting_id );
     declineUrl.searchParams.set( 'posting_id', posting_id );
+
+    acceptUrl.searchParams.set(  'profile_id', profile_id );
+    declineUrl.searchParams.set(  'profile_id', profile_id );
 
     acceptBtn.href = acceptUrl;
     declineBtn.href = declineUrl;
