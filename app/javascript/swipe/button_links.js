c4187243a341e37205707a4e5da029ae5d35f60a
Kevin Liebholz <kevin@liebholz.eu>
2019-12-17 12:30:30 +0100

    FEAT: updating answer btns after killing a card

--- app/javascript/swipe/button_links.js
+++ app/javascript/swipe/button_links.js
@@ -1,27 +1,32 @@
 import $ from 'jquery'
 
-const acceptBtn = document.getElementById("accept");
-const declineBtn = document.getElementById("decline");
+var changeLinks = (activeCardParentElement) => {
 
-const baseUrl = window.location.href.split('/')[0] + "//" + window.location.href.split('/')[2]
-const profile_id = window.location.href.split('/')[4]
+  var acceptBtn = document.getElementById("accept");
+  var declineBtn = document.getElementById("decline");
 
-const acceptUrl = new URL ("/matches/accept_decline?posting_id=jsChange&profile_id=jsChange&user_intention=accepted", baseUrl);
-const declineUrl = new URL ("/matches/accept_decline?posting_id=jsChange&profile_id=jsChange&user_intention=declined", baseUrl);
+  var baseUrl = window.location.href.split('/')[0] + "//" + window.location.href.split('/')[2]
+  var profile_id = window.location.href.split('/')[4]
 
-const changeButtonLinks = () => {
+  var acceptUrl = new URL ("/matches/accept_decline?posting_id=jsChange&profile_id=jsChange&user_intention=accepted", baseUrl);
+  var declineUrl = new URL ("/matches/accept_decline?posting_id=jsChange&profile_id=jsChange&user_intention=declined", baseUrl);
+
+  var activeCard = activeCardParentElement.querySelector(".active")
+  var posting_id = activeCard.querySelector(".swipe-card").dataset.posting
+
+  acceptUrl.searchParams.set( 'posting_id', posting_id );
+  declineUrl.searchParams.set( 'posting_id', posting_id );
+
+  acceptUrl.searchParams.set(  'profile_id', profile_id );
+  declineUrl.searchParams.set(  'profile_id', profile_id );
+
+  acceptBtn.href = acceptUrl;
+  declineBtn.href = declineUrl;
+}
+
+var changeButtonLinks = () => {
   $('#carouselExampleCaptions').on('slid.bs.carousel', function (event) {
-    let activeCard = event.currentTarget.querySelector(".active")
-    let posting_id = activeCard.querySelector(".swipe-card").dataset.posting
-
-    acceptUrl.searchParams.set( 'posting_id', posting_id );
-    declineUrl.searchParams.set( 'posting_id', posting_id );
-
-    acceptUrl.searchParams.set(  'profile_id', profile_id );
-    declineUrl.searchParams.set(  'profile_id', profile_id );
-
-    acceptBtn.href = acceptUrl;
-    declineBtn.href = declineUrl;
+    changeLinks(event.currentTarget)
   })
 }
 
